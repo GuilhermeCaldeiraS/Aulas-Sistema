10ccbfe0a2c25d570187e03bb10afe627ae32262
Guilherme Caldeira <guilhermecaldeira2703@gmail.com>
2026-09-11 21:43:01 -0300

    aula do dia 11/09/2026

---
+++ Projeto Aulas Eliesio/olamundoFreamework/olamundoFreamework/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace olamundoFreamework
+{
+    partial class Frm_Mundo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Mundo));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(304, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HelloWorld";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Frm_Mundo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = global::olamundoFreamework.Properties.Resources.world;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(767, 479);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_Mundo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MUNDO";
+            this.Load += new System.EventHandler(this.Frm_Mundo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Projeto Aulas Eliesio/olamundoFreamework/olamundoFreamework/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace olamundoFreamework
+{
+    public partial class Frm_Mundo : Form
+    {
+        public Frm_Mundo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //A orderm para aparecer as mensagens são, as primeias aspas sao a mensagem do meio, e a outra aspas sao a mensagem de cima.
+            MessageBox.Show ("Ola Mundo", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information); // mensagem para aparecer a caixa de informação e os botões respectivos, apos os cliques 
+            Application.Exit (); //fechando a Aplicação
+        }
+
+        private void Frm_Mundo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
