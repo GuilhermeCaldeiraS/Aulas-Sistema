d4e50bd55d31773ceca35978f204bf015e9a4d05
Guilherme Caldeira <guilhermecaldeira2703@gmail.com>
2026-09-14 21:35:14 -0300

    atualização da aula 14/09/2026

---
+++ Projetos Eliesio Principais/Cadastro/Cadastro/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Cadastro
+{
+    partial class frm_Cadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Cadastro));
+            this.lbl_DadosP = new System.Windows.Forms.Label();
+            this.lbl_Nome = new System.Windows.Forms.Label();
+            this.lbl_SobreNome = new System.Windows.Forms.Label();
+            this.lbl_Idade = new System.Windows.Forms.Label();
+            this.lbl_Bairro = new System.Windows.Forms.Label();
+            this.lbl_Celular = new System.Windows.Forms.Label();
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.lbl_ConfirmD = new System.Windows.Forms.Label();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_Sobrenome = new System.Windows.Forms.TextBox();
+            this.txt_Idade = new System.Windows.Forms.TextBox();
+            this.txt_Bairro = new System.Windows.Forms.TextBox();
+            this.txt_Celular = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.btn_Nome = new System.Windows.Forms.Button();
+            this.btn_Sobrenome = new System.Windows.Forms.Button();
+            this.btn_Idade = new System.Windows.Forms.Button();
+            this.btn_Bairro = new System.Windows.Forms.Button();
+            this.btn_Celular = new System.Windows.Forms.Button();
+            this.btn_Email = new System.Windows.Forms.Button();
+            this.btn_DadosC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_DadosP
+            // 
+            this.lbl_DadosP.AutoSize = true;
+            this.lbl_DadosP.Location = new System.Drawing.Point(230, 41);
+            this.lbl_DadosP.Name = "lbl_DadosP";
+            this.lbl_DadosP.Size = new System.Drawing.Size(83, 13);
+            this.lbl_DadosP.TabIndex = 0;
+            this.lbl_DadosP.Text = "Dados Pessoais";
+            // 
+            // lbl_Nome
+            // 
+            this.lbl_Nome.AutoSize = true;
+            this.lbl_Nome.Location = new System.Drawing.Point(85, 94);
+            this.lbl_Nome.Name = "lbl_Nome";
+            this.lbl_Nome.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Nome.TabIndex = 1;
+            this.lbl_Nome.Text = "Nome";
+            // 
+            // lbl_SobreNome
+            // 
+            this.lbl_SobreNome.AutoSize = true;
+            this.lbl_SobreNome.Location = new System.Drawing.Point(72, 126);
+            this.lbl_SobreNome.Name = "lbl_SobreNome";
+            this.lbl_SobreNome.Size = new System.Drawing.Size(61, 13);
+            this.lbl_SobreNome.TabIndex = 2;
+            this.lbl_SobreNome.Text = "Sobrenome";
+            // 
+            // lbl_Idade
+            // 
+            this.lbl_Idade.AutoSize = true;
+            this.lbl_Idade.Location = new System.Drawing.Point(85, 161);
+            this.lbl_Idade.Name = "lbl_Idade";
+            this.lbl_Idade.Size = new System.Drawing.Size(34, 13);
+            this.lbl_Idade.TabIndex = 3;
+            this.lbl_Idade.Text = "Idade";
+            // 
+            // lbl_Bairro
+            // 
+            this.lbl_Bairro.AutoSize = true;
+            this.lbl_Bairro.Location = new System.Drawing.Point(85, 197);
+            this.lbl_Bairro.Name = "lbl_Bairro";
+            this.lbl_Bairro.Size = new System.Drawing.Size(34, 13);
+            this.lbl_Bairro.TabIndex = 4;
+            this.lbl_Bairro.Text = "Bairro";
+            // 
+            // lbl_Celular
+            // 
+            this.lbl_Celular.AutoSize = true;
+            this.lbl_Celular.Location = new System.Drawing.Point(85, 230);
+            this.lbl_Celular.Name = "lbl_Celular";
+            this.lbl_Celular.Size = new System.Drawing.Size(39, 13);
+            this.lbl_Celular.TabIndex = 5;
+            this.lbl_Celular.Text = "Celular";
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Location = new System.Drawing.Point(85, 259);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Email.TabIndex = 6;
+            this.lbl_Email.Text = "E-mail";
+            // 
+            // lbl_ConfirmD
+            // 
+            this.lbl_ConfirmD.AutoSize = true;
+            this.lbl_ConfirmD.Location = new System.Drawing.Point(82, 304);
+            this.lbl_ConfirmD.Name = "lbl_ConfirmD";
+            this.lbl_ConfirmD.Size = new System.Drawing.Size(120, 13);
+            this.lbl_ConfirmD.TabIndex = 7;
+            this.lbl_ConfirmD.Text = "Confirmação dos Dados";
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(139, 91);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(186, 20);
+            this.txt_Nome.TabIndex = 9;
+            // 
+            // txt_Sobrenome
+            // 
+            this.txt_Sobrenome.Location = new System.Drawing.Point(139, 123);
+            this.txt_Sobrenome.Name = "txt_Sobrenome";
+            this.txt_Sobrenome.Size = new System.Drawing.Size(186, 20);
+            this.txt_Sobrenome.TabIndex = 10;
+            // 
+            // txt_Idade
+            // 
+            this.txt_Idade.Location = new System.Drawing.Point(139, 158);
+            this.txt_Idade.Name = "txt_Idade";
+            this.txt_Idade.Size = new System.Drawing.Size(48, 20);
+            this.txt_Idade.TabIndex = 11;
+            // 
+            // txt_Bairro
+            // 
+            this.txt_Bairro.Location = new System.Drawing.Point(139, 193);
+            this.txt_Bairro.Name = "txt_Bairro";
+            this.txt_Bairro.Size = new System.Drawing.Size(186, 20);
+            this.txt_Bairro.TabIndex = 12;
+            // 
+            // txt_Celular
+            // 
+            this.txt_Celular.Location = new System.Drawing.Point(139, 223);
+            this.txt_Celular.Name = "txt_Celular";
+            this.txt_Celular.Size = new System.Drawing.Size(108, 20);
+            this.txt_Celular.TabIndex = 13;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(139, 259);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(237, 20);
+            this.txt_Email.TabIndex = 14;
+            // 
+            // btn_Nome
+            // 
+            this.btn_Nome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Nome.Location = new System.Drawing.Point(413, 88);
+            this.btn_Nome.Name = "btn_Nome";
+            this.btn_Nome.Size = new System.Drawing.Size(75, 23);
+            this.btn_Nome.TabIndex = 15;
+            this.btn_Nome.Text = "Nome";
+            this.btn_Nome.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sobrenome
+            // 
+            this.btn_Sobrenome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Sobrenome.Location = new System.Drawing.Point(413, 121);
+            this.btn_Sobrenome.Name = "btn_Sobrenome";
+            this.btn_Sobrenome.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sobrenome.TabIndex = 16;
+            this.btn_Sobrenome.Text = "Sobrenome";
+            this.btn_Sobrenome.UseVisualStyleBackColor = true;
+            // 
+            // btn_Idade
+            // 
+            this.btn_Idade.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Idade.Location = new System.Drawing.Point(413, 155);
+            this.btn_Idade.Name = "btn_Idade";
+            this.btn_Idade.Size = new System.Drawing.Size(75, 23);
+            this.btn_Idade.TabIndex = 17;
+            this.btn_Idade.Text = "Idade";
+            this.btn_Idade.UseVisualStyleBackColor = true;
+            // 
+            // btn_Bairro
+            // 
+            this.btn_Bairro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Bairro.Location = new System.Drawing.Point(413, 190);
+            this.btn_Bairro.Name = "btn_Bairro";
+            this.btn_Bairro.Size = new System.Drawing.Size(75, 23);
+            this.btn_Bairro.TabIndex = 18;
+            this.btn_Bairro.Text = "Bairro";
+            this.btn_Bairro.UseVisualStyleBackColor = true;
+            // 
+            // btn_Celular
+            // 
+            this.btn_Celular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Celular.Location = new System.Drawing.Point(413, 225);
+            this.btn_Celular.Name = "btn_Celular";
+            this.btn_Celular.Size = new System.Drawing.Size(75, 23);
+            this.btn_Celular.TabIndex = 19;
+            this.btn_Celular.Text = "Celular";
+            this.btn_Celular.UseVisualStyleBackColor = true;
+            // 
+            // btn_Email
+            // 
+            this.btn_Email.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Email.Location = new System.Drawing.Point(413, 259);
+            this.btn_Email.Name = "btn_Email";
+            this.btn_Email.Size = new System.Drawing.Size(75, 23);
+            this.btn_Email.TabIndex = 20;
+            this.btn_Email.Text = "E-mail";
+            this.btn_Email.UseVisualStyleBackColor = true;
+            // 
+            // btn_DadosC
+            // 
+            this.btn_DadosC.Location = new System.Drawing.Point(413, 304);
+            this.btn_DadosC.Name = "btn_DadosC";
+            this.btn_DadosC.Size = new System.Drawing.Size(75, 50);
+            this.btn_DadosC.TabIndex = 21;
+            this.btn_DadosC.Text = "Dados Completos";
+            this.btn_DadosC.UseVisualStyleBackColor = true;
+            // 
+            // frm_Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cadastro.Properties.Resources.front_view_woman_eating_meat_burgers;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 450);
+            this.Controls.Add(this.btn_DadosC);
+            this.Controls.Add(this.btn_Email);
+            this.Controls.Add(this.btn_Celular);
+            this.Controls.Add(this.btn_Bairro);
+            this.Controls.Add(this.btn_Idade);
+            this.Controls.Add(this.btn_Sobrenome);
+            this.Controls.Add(this.btn_Nome);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.txt_Celular);
+            this.Controls.Add(this.txt_Bairro);
+            this.Controls.Add(this.txt_Idade);
+            this.Controls.Add(this.txt_Sobrenome);
+            this.Controls.Add(this.txt_Nome);
+            this.Controls.Add(this.lbl_ConfirmD);
+            this.Controls.Add(this.lbl_Email);
+            this.Controls.Add(this.lbl_Celular);
+            this.Controls.Add(this.lbl_Bairro);
+            this.Controls.Add(this.lbl_Idade);
+            this.Controls.Add(this.lbl_SobreNome);
+            this.Controls.Add(this.lbl_Nome);
+            this.Controls.Add(this.lbl_DadosP);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_Cadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_DadosP;
+        private System.Windows.Forms.Label lbl_Nome;
+        private System.Windows.Forms.Label lbl_SobreNome;
+        private System.Windows.Forms.Label lbl_Idade;
+        private System.Windows.Forms.Label lbl_Bairro;
+        private System.Windows.Forms.Label lbl_Celular;
+        private System.Windows.Forms.Label lbl_Email;
+        private System.Windows.Forms.Label lbl_ConfirmD;
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_Sobrenome;
+        private System.Windows.Forms.TextBox txt_Idade;
+        private System.Windows.Forms.TextBox txt_Bairro;
+        private System.Windows.Forms.TextBox txt_Celular;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Button btn_Nome;
+        private System.Windows.Forms.Button btn_Sobrenome;
+        private System.Windows.Forms.Button btn_Idade;
+        private System.Windows.Forms.Button btn_Bairro;
+        private System.Windows.Forms.Button btn_Celular;
+        private System.Windows.Forms.Button btn_Email;
+        private System.Windows.Forms.Button btn_DadosC;
+    }
+}
+
