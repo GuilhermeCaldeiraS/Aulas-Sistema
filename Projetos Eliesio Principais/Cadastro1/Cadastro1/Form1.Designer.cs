c65a4501b8eb0a2c61dd701a2717be51a716a8ce
Gui.Dev <guilhermecaldeira2703@gmail.com>
2026-09-16 20:02:39 -0300

    att

---
+++ Projetos Eliesio Principais/Cadastro1/Cadastro1/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace Cadastro1
+{
+    partial class Frm_Cadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Cadastro));
+            this.Lbl_DadosP = new System.Windows.Forms.Label();
+            this.Lbl_Nome = new System.Windows.Forms.Label();
+            this.Lbl_Sobrenome = new System.Windows.Forms.Label();
+            this.Lbl_idade = new System.Windows.Forms.Label();
+            this.Lbl_Bairro = new System.Windows.Forms.Label();
+            this.Lbl_Celular = new System.Windows.Forms.Label();
+            this.Lbl_email = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Txt_nome = new System.Windows.Forms.TextBox();
+            this.Txt_Sobrenome = new System.Windows.Forms.TextBox();
+            this.Txt_Idade = new System.Windows.Forms.TextBox();
+            this.Txt_Bairro = new System.Windows.Forms.TextBox();
+            this.Txt_Celular = new System.Windows.Forms.TextBox();
+            this.Btn_Nome = new System.Windows.Forms.Button();
+            this.Btn_Sobrenome = new System.Windows.Forms.Button();
+            this.Btn_Idade = new System.Windows.Forms.Button();
+            this.Btn_Bairro = new System.Windows.Forms.Button();
+            this.Btn_Celular = new System.Windows.Forms.Button();
+            this.Btn_email = new System.Windows.Forms.Button();
+            this.Txt_email = new System.Windows.Forms.TextBox();
+            this.Btn_Dcompleto = new System.Windows.Forms.Button();
+            this.Grp_Temas = new System.Windows.Forms.GroupBox();
+            this.Rad_Tema3 = new System.Windows.Forms.RadioButton();
+            this.Rad_tema2 = new System.Windows.Forms.RadioButton();
+            this.Rad_tema1 = new System.Windows.Forms.RadioButton();
+            this.Btn_Ativar = new System.Windows.Forms.Button();
+            this.Btn_Desativar = new System.Windows.Forms.Button();
+            this.Btn_Limpar = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.Grp_Temas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbl_DadosP
+            // 
+            this.Lbl_DadosP.AutoSize = true;
+            this.Lbl_DadosP.Location = new System.Drawing.Point(382, 53);
+            this.Lbl_DadosP.Name = "Lbl_DadosP";
+            this.Lbl_DadosP.Size = new System.Drawing.Size(108, 16);
+            this.Lbl_DadosP.TabIndex = 0;
+            this.Lbl_DadosP.Text = "Dados Pessoais";
+            // 
+            // Lbl_Nome
+            // 
+            this.Lbl_Nome.AutoSize = true;
+            this.Lbl_Nome.Location = new System.Drawing.Point(58, 107);
+            this.Lbl_Nome.Name = "Lbl_Nome";
+            this.Lbl_Nome.Size = new System.Drawing.Size(47, 16);
+            this.Lbl_Nome.TabIndex = 1;
+            this.Lbl_Nome.Text = "Nome ";
+            // 
+            // Lbl_Sobrenome
+            // 
+            this.Lbl_Sobrenome.AutoSize = true;
+            this.Lbl_Sobrenome.Location = new System.Drawing.Point(58, 151);
+            this.Lbl_Sobrenome.Name = "Lbl_Sobrenome";
+            this.Lbl_Sobrenome.Size = new System.Drawing.Size(78, 16);
+            this.Lbl_Sobrenome.TabIndex = 2;
+            this.Lbl_Sobrenome.Text = "Sobrenome";
+            // 
+            // Lbl_idade
+            // 
+            this.Lbl_idade.AutoSize = true;
+            this.Lbl_idade.Location = new System.Drawing.Point(58, 200);
+            this.Lbl_idade.Name = "Lbl_idade";
+            this.Lbl_idade.Size = new System.Drawing.Size(42, 16);
+            this.Lbl_idade.TabIndex = 3;
+            this.Lbl_idade.Text = "Idade";
+            // 
+            // Lbl_Bairro
+            // 
+            this.Lbl_Bairro.AutoSize = true;
+            this.Lbl_Bairro.Location = new System.Drawing.Point(58, 239);
+            this.Lbl_Bairro.Name = "Lbl_Bairro";
+            this.Lbl_Bairro.Size = new System.Drawing.Size(43, 16);
+            this.Lbl_Bairro.TabIndex = 4;
+            this.Lbl_Bairro.Text = "Bairro";
+            // 
+            // Lbl_Celular
+            // 
+            this.Lbl_Celular.AutoSize = true;
+            this.Lbl_Celular.Location = new System.Drawing.Point(58, 282);
+            this.Lbl_Celular.Name = "Lbl_Celular";
+            this.Lbl_Celular.Size = new System.Drawing.Size(49, 16);
+            this.Lbl_Celular.TabIndex = 5;
+            this.Lbl_Celular.Text = "Celular";
+            // 
+            // Lbl_email
+            // 
+            this.Lbl_email.AutoSize = true;
+            this.Lbl_email.Location = new System.Drawing.Point(58, 329);
+            this.Lbl_email.Name = "Lbl_email";
+            this.Lbl_email.Size = new System.Drawing.Size(45, 16);
+            this.Lbl_email.TabIndex = 6;
+            this.Lbl_email.Text = "E-mail";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Confirmação de Dados ";
+            // 
+            // Txt_nome
+            // 
+            this.Txt_nome.Location = new System.Drawing.Point(163, 107);
+            this.Txt_nome.Name = "Txt_nome";
+            this.Txt_nome.Size = new System.Drawing.Size(228, 22);
+            this.Txt_nome.TabIndex = 9;
+            // 
+            // Txt_Sobrenome
+            // 
+            this.Txt_Sobrenome.Location = new System.Drawing.Point(163, 148);
+            this.Txt_Sobrenome.Name = "Txt_Sobrenome";
+            this.Txt_Sobrenome.Size = new System.Drawing.Size(262, 22);
+            this.Txt_Sobrenome.TabIndex = 10;
+            // 
+            // Txt_Idade
+            // 
+            this.Txt_Idade.Location = new System.Drawing.Point(121, 196);
+            this.Txt_Idade.Name = "Txt_Idade";
+            this.Txt_Idade.Size = new System.Drawing.Size(55, 22);
+            this.Txt_Idade.TabIndex = 11;
+            // 
+            // Txt_Bairro
+            // 
+            this.Txt_Bairro.Location = new System.Drawing.Point(121, 236);
+            this.Txt_Bairro.Name = "Txt_Bairro";
+            this.Txt_Bairro.Size = new System.Drawing.Size(262, 22);
+            this.Txt_Bairro.TabIndex = 12;
+            // 
+            // Txt_Celular
+            // 
+            this.Txt_Celular.Location = new System.Drawing.Point(121, 279);
+            this.Txt_Celular.Name = "Txt_Celular";
+            this.Txt_Celular.Size = new System.Drawing.Size(164, 22);
+            this.Txt_Celular.TabIndex = 14;
+            // 
+            // Btn_Nome
+            // 
+            this.Btn_Nome.Location = new System.Drawing.Point(792, 107);
+            this.Btn_Nome.Name = "Btn_Nome";
+            this.Btn_Nome.Size = new System.Drawing.Size(76, 32);
+            this.Btn_Nome.TabIndex = 15;
+            this.Btn_Nome.Text = "Nome ";
+            this.Btn_Nome.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Sobrenome
+            // 
+            this.Btn_Sobrenome.Location = new System.Drawing.Point(792, 145);
+            this.Btn_Sobrenome.Name = "Btn_Sobrenome";
+            this.Btn_Sobrenome.Size = new System.Drawing.Size(104, 31);
+            this.Btn_Sobrenome.TabIndex = 16;
+            this.Btn_Sobrenome.Text = "Sobrenome";
+            this.Btn_Sobrenome.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Idade
+            // 
+            this.Btn_Idade.Location = new System.Drawing.Point(792, 190);
+            this.Btn_Idade.Name = "Btn_Idade";
+            this.Btn_Idade.Size = new System.Drawing.Size(76, 35);
+            this.Btn_Idade.TabIndex = 17;
+            this.Btn_Idade.Text = "Idade";
+            this.Btn_Idade.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Bairro
+            // 
+            this.Btn_Bairro.Location = new System.Drawing.Point(792, 248);
+            this.Btn_Bairro.Name = "Btn_Bairro";
+            this.Btn_Bairro.Size = new System.Drawing.Size(76, 33);
+            this.Btn_Bairro.TabIndex = 18;
+            this.Btn_Bairro.Text = "Bairro";
+            this.Btn_Bairro.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Celular
+            // 
+            this.Btn_Celular.Location = new System.Drawing.Point(792, 291);
+            this.Btn_Celular.Name = "Btn_Celular";
+            this.Btn_Celular.Size = new System.Drawing.Size(76, 34);
+            this.Btn_Celular.TabIndex = 19;
+            this.Btn_Celular.Text = "Celular";
+            this.Btn_Celular.UseVisualStyleBackColor = true;
+            // 
+            // Btn_email
+            // 
+            this.Btn_email.Location = new System.Drawing.Point(792, 338);
+            this.Btn_email.Name = "Btn_email";
+            this.Btn_email.Size = new System.Drawing.Size(83, 30);
+            this.Btn_email.TabIndex = 20;
+            this.Btn_email.Text = "E-mail";
+            this.Btn_email.UseVisualStyleBackColor = true;
+            // 
+            // Txt_email
+            // 
+            this.Txt_email.Location = new System.Drawing.Point(121, 326);
+            this.Txt_email.Name = "Txt_email";
+            this.Txt_email.Size = new System.Drawing.Size(262, 22);
+            this.Txt_email.TabIndex = 21;
+            // 
+            // Btn_Dcompleto
+            // 
+            this.Btn_Dcompleto.Location = new System.Drawing.Point(636, 370);
+            this.Btn_Dcompleto.Name = "Btn_Dcompleto";
+            this.Btn_Dcompleto.Size = new System.Drawing.Size(129, 70);
+            this.Btn_Dcompleto.TabIndex = 22;
+            this.Btn_Dcompleto.Text = "Dados\r\nCompleto\r\n";
+            this.Btn_Dcompleto.UseVisualStyleBackColor = true;
+            // 
+            // Grp_Temas
+            // 
+            this.Grp_Temas.Controls.Add(this.Rad_Tema3);
+            this.Grp_Temas.Controls.Add(this.Rad_tema2);
+            this.Grp_Temas.Controls.Add(this.Rad_tema1);
+            this.Grp_Temas.Location = new System.Drawing.Point(561, 12);
+            this.Grp_Temas.Name = "Grp_Temas";
+            this.Grp_Temas.Size = new System.Drawing.Size(379, 72);
+            this.Grp_Temas.TabIndex = 23;
+            this.Grp_Temas.TabStop = false;
+            this.Grp_Temas.Text = "Temas";
+            // 
+            // Rad_Tema3
+            // 
+            this.Rad_Tema3.AutoSize = true;
+            this.Rad_Tema3.Location = new System.Drawing.Point(251, 22);
+            this.Rad_Tema3.Name = "Rad_Tema3";
+            this.Rad_Tema3.Size = new System.Drawing.Size(71, 20);
+            this.Rad_Tema3.TabIndex = 2;
+            this.Rad_Tema3.TabStop = true;
+            this.Rad_Tema3.Text = "Tema3";
+            this.Rad_Tema3.UseVisualStyleBackColor = true;
+            this.Rad_Tema3.CheckedChanged += new System.EventHandler(this.Rad_Tema3_CheckedChanged);
+            // 
+            // Rad_tema2
+            // 
+            this.Rad_tema2.AutoSize = true;
+            this.Rad_tema2.Location = new System.Drawing.Point(133, 22);
+            this.Rad_tema2.Name = "Rad_tema2";
+            this.Rad_tema2.Size = new System.Drawing.Size(71, 20);
+            this.Rad_tema2.TabIndex = 1;
+            this.Rad_tema2.TabStop = true;
+            this.Rad_tema2.Text = "Tema2";
+            this.Rad_tema2.UseVisualStyleBackColor = true;
+            this.Rad_tema2.CheckedChanged += new System.EventHandler(this.Rad_tema2_CheckedChanged);
+            // 
+            // Rad_tema1
+            // 
+            this.Rad_tema1.AutoSize = true;
+            this.Rad_tema1.Location = new System.Drawing.Point(7, 22);
+            this.Rad_tema1.Name = "Rad_tema1";
+            this.Rad_tema1.Size = new System.Drawing.Size(71, 20);
+            this.Rad_tema1.TabIndex = 0;
+            this.Rad_tema1.TabStop = true;
+            this.Rad_tema1.Text = "Tema1";
+            this.Rad_tema1.UseVisualStyleBackColor = true;
+            this.Rad_tema1.CheckedChanged += new System.EventHandler(this.Rad_tema1_CheckedChanged);
+            // 
+            // Btn_Ativar
+            // 
+            this.Btn_Ativar.Location = new System.Drawing.Point(12, 31);
+            this.Btn_Ativar.Name = "Btn_Ativar";
+            this.Btn_Ativar.Size = new System.Drawing.Size(75, 38);
+            this.Btn_Ativar.TabIndex = 24;
+            this.Btn_Ativar.Text = "Ativar";
+            this.Btn_Ativar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Desativar
+            // 
+            this.Btn_Desativar.Location = new System.Drawing.Point(114, 31);
+            this.Btn_Desativar.Name = "Btn_Desativar";
+            this.Btn_Desativar.Size = new System.Drawing.Size(93, 38);
+            this.Btn_Desativar.TabIndex = 25;
+            this.Btn_Desativar.Text = "Desativar";
+            this.Btn_Desativar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Limpar
+            // 
+            this.Btn_Limpar.Location = new System.Drawing.Point(225, 31);
+            this.Btn_Limpar.Name = "Btn_Limpar";
+            this.Btn_Limpar.Size = new System.Drawing.Size(75, 38);
+            this.Btn_Limpar.TabIndex = 26;
+            this.Btn_Limpar.Text = "Limpar";
+            this.Btn_Limpar.UseVisualStyleBackColor = true;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(650, 135);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(69, 16);
+            this.lbl_resultado.TabIndex = 27;
+            this.lbl_resultado.Text = "Resultado";
+            // 
+            // Frm_Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cadastro1.Properties.Resources.cartoon_woman_wearing_vr_glasses;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(962, 514);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.Btn_Limpar);
+            this.Controls.Add(this.Btn_Desativar);
+            this.Controls.Add(this.Btn_Ativar);
+            this.Controls.Add(this.Grp_Temas);
+            this.Controls.Add(this.Btn_Dcompleto);
+            this.Controls.Add(this.Txt_email);
+            this.Controls.Add(this.Btn_email);
+            this.Controls.Add(this.Btn_Celular);
+            this.Controls.Add(this.Btn_Bairro);
+            this.Controls.Add(this.Btn_Idade);
+            this.Controls.Add(this.Btn_Sobrenome);
+            this.Controls.Add(this.Btn_Nome);
+            this.Controls.Add(this.Txt_Celular);
+            this.Controls.Add(this.Txt_Bairro);
+            this.Controls.Add(this.Txt_Idade);
+            this.Controls.Add(this.Txt_Sobrenome);
+            this.Controls.Add(this.Txt_nome);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Lbl_email);
+            this.Controls.Add(this.Lbl_Celular);
+            this.Controls.Add(this.Lbl_Bairro);
+            this.Controls.Add(this.Lbl_idade);
+            this.Controls.Add(this.Lbl_Sobrenome);
+            this.Controls.Add(this.Lbl_Nome);
+            this.Controls.Add(this.Lbl_DadosP);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_Cadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.Grp_Temas.ResumeLayout(false);
+            this.Grp_Temas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_DadosP;
+        private System.Windows.Forms.Label Lbl_Nome;
+        private System.Windows.Forms.Label Lbl_Sobrenome;
+        private System.Windows.Forms.Label Lbl_idade;
+        private System.Windows.Forms.Label Lbl_Bairro;
+        private System.Windows.Forms.Label Lbl_Celular;
+        private System.Windows.Forms.Label Lbl_email;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Txt_nome;
+        private System.Windows.Forms.TextBox Txt_Sobrenome;
+        private System.Windows.Forms.TextBox Txt_Idade;
+        private System.Windows.Forms.TextBox Txt_Bairro;
+        private System.Windows.Forms.TextBox Txt_Celular;
+        private System.Windows.Forms.Button Btn_Nome;
+        private System.Windows.Forms.Button Btn_Sobrenome;
+        private System.Windows.Forms.Button Btn_Idade;
+        private System.Windows.Forms.Button Btn_Bairro;
+        private System.Windows.Forms.Button Btn_Celular;
+        private System.Windows.Forms.Button Btn_email;
+        private System.Windows.Forms.TextBox Txt_email;
+        private System.Windows.Forms.Button Btn_Dcompleto;
+        private System.Windows.Forms.GroupBox Grp_Temas;
+        private System.Windows.Forms.RadioButton Rad_Tema3;
+        private System.Windows.Forms.RadioButton Rad_tema2;
+        private System.Windows.Forms.RadioButton Rad_tema1;
+        private System.Windows.Forms.Button Btn_Ativar;
+        private System.Windows.Forms.Button Btn_Desativar;
+        private System.Windows.Forms.Button Btn_Limpar;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
---
+++ Projetos Eliesio Principais/Cadastro1/Cadastro1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastro1
+{
+    public partial class Frm_Cadastro : Form
+    {
+        public Frm_Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Rad_tema1_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.Imagen_2;
+        }
+
+        private void Rad_tema2_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.imagem4;
+        }
+
+        private void Rad_Tema3_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.imagen6;
+        }
+    }
+}
